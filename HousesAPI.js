bd82a3567fc2521dc52b1b889cb1927fec1962a2
Justin Trammell <102559763+NovaPrimeGinger@users.noreply.github.com>
2022-07-09 10:46:33 -0400

    Add files via upload

---
+++ HousesAPI.js
@@ -0,0 +1,32 @@
+const HOUSES_ENDPOINT = 'http://ancient-taiga-31359.herokuapp.com/api/houses';
+
+class HousesAPI {
+  get = async () => {
+    try {
+      const resp = await fetch(HOUSES_ENDPOINT);
+      const data = await resp.json();
+      return data;
+    } catch(e) {
+      console.log('Oops, looks like fetchHouses had an issue', e)
+    }
+  }
+
+  put = async (house) => {
+    try {
+      const resp = await fetch(`${HOUSES_ENDPOINT}/${house._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(house)
+      });
+      return await resp.json();
+    } catch(e) {
+      console.log('Oops, looks like update had an issue', e);
+    }
+  }
+
+
+};
+
+export const housesAPI = new HousesAPI();
